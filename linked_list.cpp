730895e5d1af362d7d3e05016a31cb11205b2c70
Balas47 <brianalas01@gmail.com>
2020-11-04 13:03:42 -0500

    Outline of what the functionality of the linked list should be able to provide

--- linked_list.cpp
+++ linked_list.cpp
@@ -2,6 +2,36 @@
 
 template <typename T>
 struct Node{
+
+  // Member variables
   T data;
-  Node<T> next;
+  Node<T> *next;
+
+  // Helper function for linked list
+  Node<T>* getNext();
 };
+
+template <typename U>
+class LinkedList{
+private:
+  Node<U> head;
+  bool reversed;
+  int length;
+
+public:
+  LinkedList(); // Constructor
+  LinkedList(Node<U> theHead); // Overloaded Constructor
+  LinkedList(const LinkedList<U> &other); // Copy Constructor
+  LinkedList& operator= (const LinkedList<U> &other); // Assignment Operator
+  ~LinkedList(); // Destructor
+
+  // Overloaded operators
+  friend std::ostream& operator<<(std::ostream& out, const LinkedList<U> &f1);
+  Node& operator[](const unsigned int &i);
+
+  // Functions for functionality
+  bool insert(U data);
+  bool remove();
+  bool reverse();
+  bool length();
+}
